185844c661ec0f09ba587424a58cc5e3563d1862
Cookie McCormick <muntananuchat@gmail.com>
2019-04-05 13:36:35 -0400

    updated user attributes

--- db/migrate/20190404184800_create_users.rb
+++ db/migrate/20190404184800_create_users.rb
@@ -3,6 +3,7 @@ class CreateUsers < ActiveRecord::Migration[5.2]
     create_table :users do |t|
       t.string :name, null: false
       t.string :email, null: false
+      t.date :birthday, null: false
       t.string :password_digest, null: false
       t.timestamps null: false
     end
