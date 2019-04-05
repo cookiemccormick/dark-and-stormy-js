253d1a8f34a0fdf4fd340143341a55716c5c6133
Cookie McCormick <muntananuchat@gmail.com>
2019-04-05 14:58:10 -0400

    corrected login

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -4,10 +4,10 @@ class SessionsController < ApplicationController
   end
 
   def create
-    @user = User.find_by(email: params[:email])
-    if @user && @user.authenticate(params[:password])
+    @user = User.find_by(email: params[:user][:email])
+    if @user && @user.authenticate(params[:user][:password])
       session[:user_id] = @user.id
-      redirect_to user_path(@user)
+      redirect_to homepage_path
     else
       redirect_to root_path
     end
