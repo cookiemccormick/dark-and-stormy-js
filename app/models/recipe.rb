2bc67be5cdfe78d1aa9a2297818297a0965b6f59
Cookie McCormick <muntananuchat@gmail.com>
2019-04-05 14:02:26 -0400

    added validations

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -1,5 +1,9 @@
-class Cocktail < ApplicationRecord
+class Recipe < ApplicationRecord
   belongs_to :user
-  has_many :cocktail_ingredients
-  has_many :ingredients, through: cocktail_ingredients
+  has_many :recipe_ingredients
+  has_many :ingredients, through: recipe_ingredients
+
+  validates :name, presence: true, uniqueness: true
+  validates :description, presence: true
+  validates :instructions, presence: true
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,10 @@
 class User < ApplicationRecord
   has_many :recipes, dependent: :destroy
+
+  validates :name, presence: true
+  validates :email, presence: true, uniqueness: true
+  validates :birthday, presence: true
+  validates :password, presence: true
+
+  has_secure_password
 end
