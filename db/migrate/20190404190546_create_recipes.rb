f111bd0fd87294c5c7f6f57f01bf884b6f4bdc65
Cookie McCormick <muntananuchat@gmail.com>
2019-04-05 13:40:37 -0400

    corrected recipe attribute

---
+++ db/migrate/20190404190546_create_recipes.rb
@@ -0,0 +1,11 @@
+class CreateRecipes < ActiveRecord::Migration[5.2]
+  def change
+    create_table :recipes do |t|
+      t.string :name, null: false
+      t.string :description, null: false
+      t.string :instruction, null: false
+      t.integer :user_id, null: false
+      t.timestamps null: false
+    end
+  end
+end
