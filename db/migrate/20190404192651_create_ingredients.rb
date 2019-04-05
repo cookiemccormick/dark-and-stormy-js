9c311c9afd047c16df712cc5c4a21d219433f3c6
Cookie McCormick <muntananuchat@gmail.com>
2019-04-05 13:37:59 -0400

    added ingredient table and model

---
+++ db/migrate/20190404192651_create_ingredients.rb
@@ -0,0 +1,8 @@
+class CreateIngredients < ActiveRecord::Migration[5.2]
+  def change
+    create_table :ingredients do |t|
+      t.string :name null: false
+      t.timestamps null: false
+    end
+  end
+end
