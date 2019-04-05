ae1db2341378a13e2222e335752d67ec0b2f76cc
Cookie McCormick <muntananuchat@gmail.com>
2019-04-05 14:56:14 -0400

    created helper methods

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,15 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+
+  def current_user
+    @current_user ||= User.find_by(id: session[:user_id])
+  end
+
+  helper_method :current_user
+
+  private
+
+  def require_login
+    redirect_to signup_path unless current_user
+  end
 end
