d1c907f084410eb7acfab407004b3540d2444062
Cookie McCormick <muntananuchat@gmail.com>
2019-04-05 13:43:23 -0400

    corrected attributes

--- db/migrate/20190404192512_create_recipe_ingredients.rb
+++ db/migrate/20190404192512_create_recipe_ingredients.rb
@@ -3,7 +3,7 @@ class CreateRecipeIngredients < ActiveRecord::Migration[5.2]
     create_table :recipe_ingredients do |t|
       t.integer :ingredient_id, null: false
       t.integer :recipe_id, null: false
-      t.string: :quantity, null: false
+      t.string :quantity, null: false
       t.timestamps null: false
     end
   end
