b989ba15784e1945bbaf81dbbe6c5c4cec1a259a
Alisa Cookie McCormick <alisa.cookie.mccormick@gmail.com>
2019-04-13 15:20:11 -0400

    added validation for recipe name uniqueness

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -3,7 +3,7 @@ class Recipe < ApplicationRecord
   has_many :recipe_ingredients
   has_many :ingredients, through: :recipe_ingredients
 
-  validates :name, presence: true
+  validates :name, presence: true, uniqueness: true
   validates :description, presence: true
   validates :instructions, presence: true
 
