78f38d34a697057b64b18cbb27f147e06ddf20c4
Cookie McCormick <alisa.cookie.mccormick@gmail.com>
2019-04-15 17:37:15 -0400

    added link to home and updated route name

--- config/routes.rb
+++ config/routes.rb
@@ -6,7 +6,7 @@ Rails.application.routes.draw do
   post '/login', to: 'sessions#create'
   post '/logout', to: 'sessions#destroy'
 
-  get '/homepage', to: 'welcome#new'
+  get '/home', to: 'welcome#new'
 
   resources :recipes
   resources :ingredients
