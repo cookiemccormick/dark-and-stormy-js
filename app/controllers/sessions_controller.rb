42ae11b36b2ea0c1ccfb704114f2df84a7f22755
Cookie McCormick <alisa.cookie.mccormick@gmail.com>
2019-04-15 17:45:42 -0400

    updated home path

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -7,7 +7,7 @@ class SessionsController < ApplicationController
     @user = User.find_by(email: params[:user][:email])
     if @user && @user.authenticate(params[:user][:password])
       session[:user_id] = @user.id
-      redirect_to homepage_path
+      redirect_to home_path
     else
       redirect_to root_path
     end
