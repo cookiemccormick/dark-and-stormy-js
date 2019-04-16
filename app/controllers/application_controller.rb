9561a11d89b969aafee2d7f7f859f9686f994256
Cookie McCormick <alisa.cookie.mccormick@gmail.com>
2019-04-16 14:20:10 -0400

    added before_action

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,6 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+  before_action :require_login
 
   def current_user
     @current_user ||= User.find_by(id: session[:user_id])
