88c07e9bcf0f1eb82c30e8fc1777f59361a30cf1
Cookie McCormick <alisa.cookie.mccormick@gmail.com>
2019-04-25 14:19:29 -0400

    added comment count logic to model

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -5,4 +5,14 @@ class Comment < ApplicationRecord
   validates :body, presence: true, allow_blank: false
   validates :recipe_id, presence: true
   validates :user_id, presence: true
+
+  def self.comment_count
+    if self.count == 0
+      "This recipe has no comments"
+    elsif self.count == 1
+      "1 comment"
+    else
+      "#{self.count} comments"
+    end
+  end
 end
