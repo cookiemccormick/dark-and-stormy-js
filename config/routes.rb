aa2b455838682f2b2ec1ffd4dc8af7fece444ca6
Cookie McCormick <alisa.cookie.mccormick@gmail.com>
2019-04-26 14:11:51 -0400

    updated welcome route

--- config/routes.rb
+++ config/routes.rb
@@ -6,7 +6,7 @@ Rails.application.routes.draw do
   post '/login', to: 'sessions#create'
   post '/logout', to: 'sessions#destroy'
 
-  get '/home', to: 'welcome#new'
+  get '/home', to: 'welcome#home'
 
   resources :recipes
   resources :ingredients
