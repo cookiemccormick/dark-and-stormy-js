ade93a2db1e6eb53535265c6de23b28f59189772
github username <github email address>
2019-05-30 16:06:34 -0400

    deleted comment.js and recipe.js

--- app/assets/javascripts/comment.js
+++
@@ -1,7 +0,0 @@
-class Comment {
-  constructor(body, recipe_id, user_id) {
-    this.body = body;
-    this.recipe_id = recipe_id;
-    this.user_id = user_id;
-  }
-}
--- app/assets/javascripts/recipe.js
+++
@@ -1,8 +0,0 @@
-class Recipe {
-  constructor(name, description, instructions, user_id) {
-    this.name = name;
-    this.description = description;
-    this.instructions = instructions;
-    this.user_id = user_id;
-  }
-}
