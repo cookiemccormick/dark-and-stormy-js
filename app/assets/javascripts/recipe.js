8b1abb0accae8e5c83a0fc0e32dcc4b4006aca84
github username <github email address>
2019-05-30 15:30:17 -0400

    created javascript object classes for recipe and comment

---
+++ app/assets/javascripts/recipe.js
@@ -0,0 +1,8 @@
+class Recipe {
+  constructor(name, description, instructions, user_id) {
+    this.name = name;
+    this.description = description;
+    this.instructions = instructions;
+    this.user_id = user_id;
+  }
+}
