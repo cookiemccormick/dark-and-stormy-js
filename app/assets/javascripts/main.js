14b2442438de144ff469a4d601f85802098635a4
github username <github email address>
2019-05-31 20:10:50 -0400

    created main.js and updated recipe index view

---
+++ app/assets/javascripts/main.js
@@ -0,0 +1,24 @@
+function getAllRecipes() {
+  $.get('/recipes.json').done(function(data) {
+    const recipes = data.map(json => new Recipe(json));
+
+    if (recipes.length === 0) {
+      $("#recipes").html("<p>There are currently no recipes.</p>");
+      return;
+    }
+
+    const recipeList = recipes.map(function(recipe) {
+      return `
+      <li>
+        <a href="/recipes/${recipe.id}">${recipe.name}</a>
+        created ${recipe.createdAt.toLocaleDateString()}<br>
+        ${recipe.description}<br>
+        by <a href="/users/${recipe.user.id}">${recipe.user.name}</a><br><br>
+      </li>`;
+    })
+
+    $("#recipes").html(`<ul>${recipeList.join('')}</ul>`);
+  });
+}
+
+$(getAllRecipes);
