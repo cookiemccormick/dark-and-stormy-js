798dcf33662ef6506dc880827b993a0fd2cd592e
github username <github email address>
2019-05-31 20:10:02 -0400

    removed comments

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -4,19 +4,13 @@ class Recipe < ApplicationRecord
   has_many :ingredients, through: :recipe_ingredients
   has_many :comments, dependent: :destroy
 
-  #Validations - special method calls that go in the model class and prevent them from being saved to the database if invalid
   validates :name, presence: true, uniqueness: true
   validates :description, presence: true
   validates :instructions, presence: true
-  #Validate - custom validator for quick custom validations that you can extract later
   validate :validate_recipe_ingredients
 
-  #lets you create and update related and child objects in one operation
-  #rejects the row if the quantity and ingredient is blank
   accepts_nested_attributes_for :recipe_ingredients, reject_if: proc {|attr| attr[:quantity].blank? && attr[:ingredient_attributes][:name].blank?}
 
-  #scoping - allows you to specify commonly-used queries which can be referenced as class method calls on the association/models
-  #ActiveRecord Query Methods - ex. find, from, group, having, includes, limit, order, select, where
   scope :most_recent, -> (limit) { order("created_at desc").limit(limit) }
   scope :alphabetical_name, -> { order("name asc") }
 
@@ -26,7 +20,6 @@ class Recipe < ApplicationRecord
     end
   end
 
-  #creating 10 empty rows for recipe_ingredeints to help with the new/edit view
   def build_empty_ingredients
     10.times { recipe_ingredients.build.build_ingredient }
   end
