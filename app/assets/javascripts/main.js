9bbb43dc9057e265176bdc790cf7a3df02c56f31
github username <github email address>
2019-06-03 13:26:42 -0400

    updated home view to load recent recipes through JavaScript and JSON

--- app/assets/javascripts/main.js
+++ app/assets/javascripts/main.js
@@ -22,3 +22,27 @@ function getAllRecipes() {
 }
 
 $(getAllRecipes);
+
+function getRecentRecipes() {
+  $.get('/home.json').done(function(data) {
+    const recipes = data.map(json => new Recipe(json));
+
+    if (recipes.length === 0) {
+      $("#recentRecipes").html("<p>There are currently no recipes.</p>");
+      return;
+    }
+
+    const recipeList = recipes.map(function(recipe) {
+      return `
+      <li>
+        <a href="/recipes/${recipe.id}">${recipe.name}</a> -
+        created ${recipe.createdAt.toLocaleDateString()}
+        by ${recipe.user.name}
+      </li>`;
+    })
+
+    $("#recentRecipes").html(`<ul>${recipeList.join('')}</ul>`);
+  });
+}
+
+$(getRecentRecipes);
--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,5 +1,9 @@
 class WelcomeController < ApplicationController
   def home
     @recipes = Recipe.all.most_recent(5)
+    respond_to do |format|
+      format.html { render :home }
+      format.json { render json: @recipes }
+    end
   end
 end
