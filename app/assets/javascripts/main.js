c9293df7ec70098ea81c25627c8977fd909f063f
github username <github email address>
2019-06-04 00:05:31 -0400

    updated recipe functions to check page that it's on and created function for recipe show view

--- app/assets/javascripts/main.js
+++ app/assets/javascripts/main.js
@@ -1,4 +1,8 @@
 function getAllRecipes() {
+  if ($("#recipes").length === 0) {
+    return;
+  }
+
   $.get('/recipes.json').done(function(data) {
     const recipes = data.map(json => new Recipe(json));
 
@@ -24,6 +28,10 @@ function getAllRecipes() {
 $(getAllRecipes);
 
 function getRecentRecipes() {
+  if ($("#recentRecipes").length === 0) {
+    return;
+  }
+
   $.get('/home.json').done(function(data) {
     const recipes = data.map(json => new Recipe(json));
 
@@ -46,3 +54,70 @@ function getRecentRecipes() {
 }
 
 $(getRecentRecipes);
+
+function showRecipe() {
+  if ($("#showRecipeData").length === 0) {
+    return;
+  }
+
+  const recipeId = $("#showRecipeData").attr("data-id");
+
+  $.get(`/recipes/${recipeId}.json`).done(function(json) {
+    const recipe = new Recipe(json);
+
+    const recipeIngredients = recipe.ingredients.map(function(ingredient) {
+      return `
+      <tr>
+        <td>${ingredient.quantity}</td>
+        <td>${ingredient.name}</td>
+      </tr>`;
+    });
+
+    let commentCount = null;
+
+    if (recipe.comments.length === 0) {
+      commentCount = "<p>There are no comments.</p>";
+    } else if (recipe.comments.length === 1) {
+      commentCount = "<p>1 comment</p>";
+    } else {
+      commentCount = `<p>${recipe.comments.length} comment</p>`;
+    }
+
+    const comments = recipe.comments.map(function(comment) {
+      return `
+      <li>
+        ${comment.body} created on ${comment.createdAt.toLocaleDateString()} by ${comment.commenter}
+      </li>`;
+    })
+
+    const html = `
+      <h1>${recipe.name}</h1>
+      <h4>Created ${recipe.createdAt.toLocaleDateString()}<br> by <a href="/${recipe.user.name}/${recipe.user.id}">${recipe.user.name}</a></h4>
+      <h3>About the ${recipe.name} cocktail</h3>
+      <p>${recipe.description}</p>
+      <h3>Ingredients in the ${recipe.name} cocktail</h3>
+      <table>
+        <thead>
+          <tr>
+            <th>Quantity</th>
+            <th>Ingredients</th>
+          </tr>
+        </thead>
+
+        <tbody>
+          ${recipeIngredients.join('')}
+        </tbody>
+      </table>
+
+      <h3>How to make the ${recipe.name}</h3>
+      <p>${recipe.instructions}</p>
+
+      <h3>Comments:</h3>
+      <div>${commentCount}</div><br>
+      <div>${comments}</div>`;
+
+    $("#showRecipeData").html(html);
+  });
+}
+
+$(showRecipe);
