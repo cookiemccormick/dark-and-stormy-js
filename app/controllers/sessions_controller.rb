6f4363973487fb4d6dcb05654f6eeecc7c56b322
github username <github email address>
2019-06-10 14:34:29 -0400

    added welcome message on home view

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -16,6 +16,7 @@ class SessionsController < ApplicationController
       @user = User.find_by(email: params[:session][:email])
       if @user && @user.authenticate(params[:session][:password])
         session[:user_id] = @user.id
+        flash[:message] = "Welcome!"
         redirect_to home_path
       else
         flash[:message] = "There was an error, please try again."
